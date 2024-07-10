31ab9bc8e0a7f3f3b8d938f5960c1aa47cafe759
Laila <lailaalmoataz99@gmail.com>
2024-07-11 01:22:22 +0300

    sql_load

---
+++ Case Study #1 - Danny's Diner/sql_load/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE sales (
+    "customer_id" VARCHAR(1),
+    "order_date" DATE,
+    "product_id" INTEGER
+);
+CREATE TABLE menu (
+    "product_id" INTEGER,
+    "product_name" VARCHAR(5),
+    "price" INTEGER
+);
+CREATE TABLE members (
+    "customer_id" VARCHAR(1),
+    "join_date" DATE
+);
